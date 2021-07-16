23b7e43728a70343968479743e585ad2c4e12565
viniciosgois <viniciosgois17@gmail.com>
2021-07-15 21:19:25 -0400

    first repo and first application

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  //add TASK
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  //delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //TOGGLE REMINDER
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
+  }
+
+  return (
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask
+          (!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+      )}
+    </div>
+
+  );
+}
+
+export default App;
